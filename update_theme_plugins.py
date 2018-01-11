4105087ac8484a9db3371a498e8cd5164b729c7c
Wafer Li <omyshokami@gmail.com>
2018-01-11 18:42:32 +0900

    Fix update script

--- update_theme_plugins.py
+++ update_theme_plugins.py
@@ -3,9 +3,18 @@
 import os
 from subprocess import call
 
+from typing import List
+
 if __name__ == '__main__':
-    root_dir = 'themes/next-reloaded/source/lib'
+    root_dir = os.path.abspath('themes/next-reloaded/source/lib')
+    git_dirs: List[str] = []
 
     for dir_path, dir_names, files in os.walk(root_dir):
         if '.git' in dir_names:
-            call(['git', '--git-dir', os.path.join(dir_path, '.git'), 'pull'])
+            git_dirs.append(dir_path)
+
+    for git_dir in git_dirs:
+        os.chdir(git_dir)
+        print(os.path.basename(git_dir))
+        call(['git', 'pull'])
+        print()
