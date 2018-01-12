8b9740577c841f7363607628876e5d8695a10250
Wafer Li <omyshokami@gmail.com>
2018-01-12 14:51:14 +0900

    Fix not working on old python3

--- git_reset_mtime.py
+++ git_reset_mtime.py
@@ -2,17 +2,23 @@
 
 import subprocess
 import os
+import shlex
 
 if __name__ != '__main__':
     raise ImportError("%s should not be used as a module." % __name__)
 
-magic_cmd = 'git ls-files -z | xargs -0 -n1 -I{} -- git log -1 --format="%ct {}" {} | sort'
+# 'git ls-files -z | xargs -0 -n1 -I{} -- git log -1 --format="%ct {}" {} | sort'
+git_ls_cmd = 'git ls-files -z'
+xargs_cmd = 'xargs -0 -n1 -I{} -- git log -1 --format="%ct {}" {}'
+sort_cmd = 'sort'
 
 work_dir = os.getcwd()
 
-result = subprocess.run(magic_cmd, stdout=subprocess.PIPE, shell=True)
+git_ls_result = subprocess.Popen(shlex.split(git_ls_cmd), stdout=subprocess.PIPE)
+xargs_result = subprocess.Popen(shlex.split(xargs_cmd), stdin=git_ls_result.stdout, stdout=subprocess.PIPE)
+result = subprocess.check_output('sort', stdin=xargs_result.stdout)
 
-timestamp_file_list = [tuple(it.split(' ', 1)) for it in result.stdout.decode('utf-8').split('\n')][:-1]
+timestamp_file_list = [tuple(it.split(' ', 1)) for it in result.decode('utf-8').split('\n')][:-1]
 
 for timestamp, file_path in timestamp_file_list:
     os.utime(os.path.join(work_dir, file_path), (int(timestamp), int(timestamp)))
