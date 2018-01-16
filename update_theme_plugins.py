0eb1422ff277088a8d39394ba7993df03d3b6511
Wafer Li <omyshokami@gmail.com>
2018-01-17 00:22:22 +0900

    Local Script: Auto clone theme lib if they doesn't exist

--- update_theme_plugins.py
+++ update_theme_plugins.py
@@ -1,20 +1,28 @@
 #!usr/bin/env python3
 
 import os
+import shlex
 from subprocess import call
 
 from typing import List
 
 if __name__ == '__main__':
-    root_dir = os.path.abspath('themes/next-reloaded/source/lib')
+    theme_root = os.path.abspath('themes/next-reloaded')
+    theme_lib_root = os.path.abspath('themes/next-reloaded/source/lib')
     git_dirs: List[str] = []
 
-    for dir_path, dir_names, files in os.walk(root_dir):
+    for dir_path, dir_names, files in os.walk(theme_lib_root):
         if '.git' in dir_names:
             git_dirs.append(dir_path)
 
-    for git_dir in git_dirs:
-        os.chdir(git_dir)
-        print(os.path.basename(git_dir))
-        call(['git', 'pull'])
-        print()
+    if len(git_dirs) == 0:
+        os.chdir(theme_root)
+        call(shlex.split('git clone https://github.com/theme-next/theme-next-canvas-nest source/lib/canvas-nest'))
+        call(shlex.split('git clone https://github.com/theme-next/theme-next-fancybox3 source/lib/fancybox'))
+        call(shlex.split('git clone https://github.com/theme-next/theme-next-reading-progress source/lib/reading_progress'))
+    else:
+        for git_dir in git_dirs:
+            os.chdir(git_dir)
+            print(os.path.basename(git_dir))
+            call(['git', 'pull'])
+            print()
