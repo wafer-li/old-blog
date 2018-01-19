148b53c61106382fe5fcd4d5274ec902e9435a54
Wafer Li <omyshokami@gmail.com>
2018-01-19 22:37:08 +0900

    Add pangu support

--- update_theme_plugins.py
+++ update_theme_plugins.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
         call(shlex.split('git clone https://github.com/theme-next/theme-next-canvas-nest source/lib/canvas-nest'))
         call(shlex.split('git clone https://github.com/theme-next/theme-next-fancybox3 source/lib/fancybox'))
         call(shlex.split('git clone https://github.com/theme-next/theme-next-reading-progress source/lib/reading_progress'))
+        call(shlex.split('git clone https://github.com/theme-next/theme-next-pangu.git source/lib/pangu'))
     else:
         for git_dir in git_dirs:
             os.chdir(git_dir)
