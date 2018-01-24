c484a18c1a624b3bf72a1961cc0b0d62fd3a3c51
Wafer Li <omyshokami@gmail.com>
2018-01-24 20:24:55 +0900

    Refactor python script

--- update_theme_plugins.py
+++ update_theme_plugins.py
@@ -1,32 +1,38 @@
 #!usr/bin/env python3
 
+'''
+Automatically install and update theme plugins
+Which is git submodules.
+'''
+
 import os
 import shlex
 from subprocess import call
 
-dependencies_calls = {
+DEPENDENCIES = {
     'reading_progress': 'git clone https://github.com/theme-next/theme-next-reading-progress ' +
                         'source/lib/reading_progress',
-    'bookmark': 'git clone https://github.com/theme-next/theme-next-bookmark.git source/lib/bookmark'
+    'bookmark': 'git clone https://github.com/theme-next/theme-next-bookmark.git ' +
+                'source/lib/bookmark'
 }
 
 if __name__ == '__main__':
-    theme_root = os.path.abspath('themes/next-reloaded')
-    theme_lib_root = os.path.abspath('themes/next-reloaded/source/lib')
-    git_dirs = []
+    THEME_ROOT = os.path.abspath('themes/next-reloaded')
+    THEME_LIB_ROOT = os.path.abspath('themes/next-reloaded/source/lib')
+    GIT_DIRS = []
 
-    for dir_path, dir_names, files in os.walk(theme_lib_root):
+    for dir_path, dir_names, files in os.walk(THEME_LIB_ROOT):
         if '.git' in dir_names:
-            git_dirs.append(dir_path)
+            GIT_DIRS.append(dir_path)
 
-    base_git_dirs = [os.path.basename(os.path.normpath(it)) for it in git_dirs]
-    install_git_dirs = set(dependencies_calls.keys()) - set(base_git_dirs)
+    BASE_GIT_DIRS = [os.path.basename(os.path.normpath(it)) for it in GIT_DIRS]
+    INSTALL_GIT_DIRS = set(DEPENDENCIES.keys()) - set(BASE_GIT_DIRS)
 
-    for it in install_git_dirs:
-        os.chdir(theme_root)
-        call(shlex.split(dependencies_calls[it]))
+    for it in INSTALL_GIT_DIRS:
+        os.chdir(THEME_ROOT)
+        call(shlex.split(DEPENDENCIES[it]))
 
-    for git_dir in git_dirs:
+    for git_dir in GIT_DIRS:
         os.chdir(git_dir)
         print(os.path.basename(git_dir))
         call(['git', 'pull'])
