3c3d87df19258fad223636d79e50908fed3c0429
Wafer Li <omyshokami@gmail.com>
2021-07-15 20:10:17 +0800

    fix: sidebar not contain the latest post

--- src/.vuepress/config/sideBar/blog-corners/tech/android/traps.js
+++ src/.vuepress/config/sideBar/blog-corners/tech/android/traps.js
@@ -1,6 +1,7 @@
 const { sidebarConfig } = require("vuepress-theme-hope");
 
 module.exports = sidebarConfig([
+    "记一次-camerax-surfacecombination-的折腾",
     "comparison-method-violates-its-general-contract-崩溃分析",
     "android-高德地图轨迹回放",
     "android-测试坑点详解（二）——-vectordrawable-和-tint-问题解析",
